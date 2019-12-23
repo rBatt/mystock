808503c8d73c20a50a5b4372e047704608453cbc
rBatt <battrd@gmail.com>
2019-12-23 15:47:06 -0500

    update setup imports, license, notes

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 # !/usr/bin/env python
-
-from distutils.core import setup
-setup(
+# python setup.py bdist_wheel
+import setuptools
+setuptools.setup(
     name='mystock',
-    packages=[],
+    packages=setuptools.find_packages(),
     version='0.0.0',
     description='Provides updates and information on select financial instruments',
     author='Ryan D. Batt',
@@ -11,11 +11,10 @@ setup(
     author_email='battrd@gmail.com',
     url='https://github.com/rBatt/mystock',
     keywords=['finance', 'schedule', 'package', ],
-    python_requires='>=3.6',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Natural Language :: English',
-        'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.6',
     ],
