bec1e986635c44c31fd7e9e0c6942e727ecc2676
rBatt <battrd@gmail.com>
2019-12-24 10:01:53 -0500

    create input patch in setUp so it can be used in other modules

--- tests/test_apicall.py
+++ tests/test_apicall.py
@@ -9,19 +9,20 @@ from mystock.apicall import ApiCall
 class TestApiCall(unittest.TestCase):
 
     def setUp(self):
+        self.input_patch = patch('mystock.apicall.input')
+        self.input_mock = self.input_patch.start()
+        self.input_mock.return_value = "testKey"
+
         self.call1 = ApiCall()
-        # self.call1.api_key = 'testKey'
         self.call1.keyfile = re.sub(r"apikey.txt", "apikey_test.txt", self.call1.keyfile)
 
         self.call2 = ApiCall()
 
     def tearDown(self):
         os.remove(self.call1.keyfile)
+        self.input_patch.stop()
 
-    @patch('mystock.apicall.input')
-    def test_get_key(self, mock_input):
-        # test getting user input and writing to file
-        mock_input.return_value = "testKey"
+    def test_get_key(self):
         self.assertFalse(os.path.exists(self.call1.keyfile)) # test key file should not exist
         self.assertEqual(self.call1._get_key(), 'testKey') # mock prompt for key and retrieval
         self.assertTrue(os.path.exists(self.call1.keyfile)) # test that apikey file exists
