217862c9f9c4d73750b7544c5452f464f60d095d
rBatt <battrd@gmail.com>
2019-12-31 18:41:10 -0500

    add skeleton test for new stats module

---
+++ tests/test_stats.py
@@ -0,0 +1,12 @@
+import unittest
+from unittest.mock import patch
+
+from pandas.util.testing import assert_frame_equal
+
+from mystock import stats
+
+class TestApiCall(unittest.TestCase):
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
